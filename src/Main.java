8453e3370f8ba22b9d364522f38dd266a517e1e0
NestJul <Nest-Ju@ya.ru>
2024-11-15 00:22:27 +0700

    2

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int initial = 100; // состояние счета
+        int replenishment = 1100; //сумма пополнения
+
+        int x= 300;
+
+        if (replenishment < 1000) {
+            System.out.println((initial + replenishment)+ " состояние лицевого счета ");
+        }
+        else {
+            System.out.println(( replenishment / 100 + initial + replenishment)+ " состояние лицевого счета");
+        }
+
+
+
+
+    }
+}
